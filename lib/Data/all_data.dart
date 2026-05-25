2cdfc574b11d87cbf544fef7b3e1440b45a1eb98
Siddhartha-4091 <gs092502@gmail.com>
2026-05-25 19:53:13 +0530

    next changes

---
+++ lib/Data/all_data.dart
@@ -0,0 +1,18 @@
+// ignore: file_names
+const List<Map<String, int>> nums = [
+  {"Sagar": 101, "Gaurav": 102, "Neha": 103}, // List index = 0
+  {"Sagar": 104, "Sunita": 105, "Anju": 106}, // List index = 1
+  {"Vishal": 107, "Ravi": 108, "disu": 109}, // List index = 2
+  {"Saurav": 110, "Siddhartha": 111, "Priyanshu": 112}, // List index = 3
+];
+
+final List<Map<String, String>> _genders = [
+  {"Sagar": "male", "Gaurav": "male", "Neha": "female"}, // List index = 0
+  {"Priya": "female", "Sunita": "female", "Anju": "female"}, // List index = 1
+  {"Vishal": "male", "Ravi": "male", "dishu": "male"}, // List index = 2
+  {
+    "Saurav": "male",
+    "Siddhartha": "male",
+    "Priyanshu": "male",
+  }, // List index = 3
+];
