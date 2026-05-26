358387fe1310c3d929591d961a2b2112dd3ac05f
Siddhartha-4091 <gs092502@gmail.com>
2026-05-26 13:47:33 +0530

    next change

--- lib/Data/all_data.dart
+++ lib/Data/all_data.dart
@@ -16,3 +16,31 @@ final List<Map<String, String>> genders = [
     "Priyanshu": "male",
   }, // List index = 3
 ];
+
+final List<Map<String, String>> quotes = [
+  {
+    "Sagar": "The only way to do great work is to love what you do.",
+    "Gaurav":
+        "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    "Neha": "Believe you can and you're halfway there.",
+  }, // List index = 0
+  {
+    "Priya": "Don't watch the clock; do what it does. Keep going.",
+    "Sunita":
+        "The future belongs to those who believe in the beauty of their dreams.",
+    "Anju": "It does not matter how slowly you go as long as you do not stop.",
+  }, // List index = 1
+  {
+    "Vishal": "The best way to predict the future is to invent it.",
+    "Ravi": "You miss 100% of the shots you don't take.",
+    "dishu":
+        "The only limit to our realization of tomorrow will be our doubts of today.",
+  }, // List index = 2
+  {
+    "Saurav":
+        "The harder you work for something, the greater you'll feel when you achieve it.",
+    "Siddhartha": "Dream big and dare to fail.",
+    "Priyanshu":
+        "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.",
+  }, // List index = 3
+];
